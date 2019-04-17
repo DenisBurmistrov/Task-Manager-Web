c5bbd7705acc2125cd4276309c8d3b8893f51903
DenisBurmistrov <6y6yka1997@gmail.com>
2019-04-17 12:41:23 +0300

    added jpa config

--- src/main/java/ru/burmistrov/taskManager/configure/WebSecurityConfig.java
+++ src/main/java/ru/burmistrov/taskManager/configure/WebSecurityConfig.java
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,11 +23,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
 
 @EnableWebMvc
 @Configuration
@@ -28,10 +41,16 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebSecurity
 @EnableGlobalAuthentication
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+@EnableJpaRepositories("ru.burmistrov.taskManager.api.repository")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -80,5 +99,46 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
         resolver.setPrefix("/WEB-INF/jsp/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource =
+                new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("application.driver")));
+        dataSource.setUrl(env.getProperty("application.url"));
+        dataSource.setUsername(env.getProperty("application.user"));
+        dataSource.setPassword(env.getProperty("application.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            final DataSource dataSource
+    ) {
+        final LocalContainerEntityManagerFactoryBean factoryBean;
+        factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setPackagesToScan("ru.evseenko");
+
+        final Properties properties = new Properties();
+        properties.put("hibernate.show_sql", Objects.requireNonNull(env.getProperty("application.show_sql")));
+        properties.put("hibernate.hbm2ddl.auto", Objects.requireNonNull(env.getProperty("application.hbm2ddl.auto")));
+        properties.put("hibernate.dialect", Objects.requireNonNull(env.getProperty("application.dialect")));
+        factoryBean.setJpaProperties(properties);
+
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(
+            final LocalContainerEntityManagerFactoryBean emf
+    ) {
+        final JpaTransactionManager transactionManager =
+                new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf.getObject());
+        return transactionManager;
     }
 }
